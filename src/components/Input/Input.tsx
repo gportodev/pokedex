152c916d1f786edac9d207c0bea18802452331f3
gportodev <gportodev@gmail.com>
2024-11-25 09:38:32 -0300

    ref: remove search function

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Alert, Keyboard, TextInput, View } from 'react-native';
+import { TextInput, View } from 'react-native';
 import style from './style';
-import api from '@/services/api';
 import theme from '@/style/theme';
 import { Ionicons } from '@expo/vector-icons';
 
@@ -11,42 +10,6 @@ type Props = {
 };
 
 function Input({ wantedPokemon, setWantedPokemon }: Props): JSX.Element {
-  // const searchPoke = () => {
-  //   if (!wantedPokemon) {
-  //     Alert.alert('Type the pokemon name!');
-  //   } else {
-  //     const convert = String(wantedPokemon).toLowerCase();
-
-  //     (async () => {
-  //       try {
-  //         const response = await api.get(`pokemon/${convert}`);
-  //         const { id, name, height, weight, types, sprites, stats } = response.data;
-
-  //         if (response.data) {
-  //           const pok = {
-  //             i: String(id),
-  //             n: name,
-  //             h: height,
-  //             w: weight,
-  //             t: types,
-  //             s: sprites.front_default,
-  //             hp: stats[0].base_stat,
-  //             atk: stats[1].base_stat,
-  //             def: stats[2].base_stat,
-  //             spd: stats[3].base_stat,
-  //           };
-
-  //           navigation.navigate('Detail', { item: pok });
-  //         }
-  //       } catch (error) {
-  //         Alert.alert('Error!', 'The pokemon does not exist!');
-  //         Keyboard.dismiss();
-  //         setWantedpokemon('');
-  //       }
-  //     })();
-  //   }
-  // };
-
   return (
     <View style={style.container}>
       <View style={style.icon}>
@@ -60,7 +23,6 @@ function Input({ wantedPokemon, setWantedPokemon }: Props): JSX.Element {
         autoCapitalize="none"
         onChangeText={text => setWantedPokemon(text)}
         value={wantedPokemon}
-        // onSubmitEditing={searchPoke}
         style={style.input}
       />
     </View>
