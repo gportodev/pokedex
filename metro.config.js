5b793c00dd9f0a02cc834c49c9c7dad0614aa4bd
gportodev <gportodev@gmail.com>
2024-12-14 14:40:21 -0300

    build: rollback sdk

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,3 @@
-const {
-  wrapWithReanimatedMetroConfig,
-} = require('react-native-reanimated/metro-config');
-
 const { getDefaultConfig } = require('expo/metro-config');
 const config = getDefaultConfig(__dirname);
 const { transformer, resolver } = config;
@@ -16,4 +12,4 @@ config.resolver = {
   sourceExts: [...resolver.sourceExts, 'svg'],
 };
 
-module.exports = wrapWithReanimatedMetroConfig(config);
+module.exports = config;
